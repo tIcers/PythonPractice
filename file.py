baadd82e792c0959da8ce3cedb5b8e1ba0a429cf
Atsuki Uchida <csatsfapsfu2930@gmail.com>
2022-04-20 22:52:09 -0700

    Deleted contents for class, file and funciton.py

--- file.py
+++ file.py
@@ -1,10 +1,15 @@
-file_name = "little_women.txt"
+def main():
+    file_name = "little_women.txt"
 
-with open(file_name) as file_object:
-    lines = file_object.read()
-count = 0
-for line in lines:
-    if "A" in line:
-        count += 1
+    with open(file_name) as file_object:
+        content = file_object.read()
+    count = 0
+    for line in content:
+        if "A" in line:
+            count += 1
 
-print(f"number of A are {count}")
+    print(f"number of A's is {count}")
+
+
+if __name__ == "__main__":
+    main()
