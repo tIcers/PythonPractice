2753566934b5e9fe588d70a2f381369b8f3c88d0
Atsuki Uchida <csatsfapsfu2930@gmail.com>
2022-05-01 15:28:38 -0700

    solved has_33, paper_doll, blackjack and summer problems

--- method&function.py
+++ method&function.py
@@ -63,3 +63,72 @@ def almost_there(n):
         return True
     else:
         return False
+
+
+"""FIND 33:
+Given a list of ints, return True if the array contains a 3 next to a 3 somewhere.
+
+has_33([1, 3, 3]) → True
+has_33([1, 3, 1, 3]) → False
+has_33([3, 1, 3]) → False"""
+
+
+def has_33(nums):
+    for num in range(0, len(nums) - 1):
+        if nums[num] == 3 and nums[num + 1] == 3:
+            return True
+    return False
+
+
+"""
+PAPER DOLL: Given a string, return a string where for every character in the original there are three characters
+paper_doll('Hello') --> 'HHHeeellllllooo'
+paper_doll('Mississippi') --> 'MMMiiissssssiiippppppiii'"""
+
+
+def paper_doll(text):
+    result = ''
+    for char in text:
+        result += char * 3
+    return result
+
+
+"""BLACKJACK: Given three integers between 1 and 11, if their sum is less than or equal to 21, return their sum. If 
+their sum exceeds 21 and there's an eleven, reduce the total sum by 10. Finally, if the sum (even after adjustment) 
+exceeds 21, return 'BUST' blackjack(5,6,7) --> 18 blackjack(9,9,9) --> 'BUST' blackjack(9,9,11) --> 19 """
+
+
+def blackjack(*nums):
+    result = sum(nums)
+    if result <= 21:
+        return result
+    elif result > 21 and 11 in nums:
+        result -= 10
+        return result
+    else:
+        return 'BUST'
+
+
+"""SUMMER OF '69: Return the sum of the numbers in the array, except ignore sections of numbers starting with a 6 and extending to the next 9 (every 6 will be followed by at least one 9). Return 0 for no numbers.
+summer_69([1, 3, 5]) --> 9
+summer_69([4, 5, 6, 7, 8, 9]) --> 9
+summer_69([2, 1, 6, 9, 11]) --> 14"""
+
+
+def summer_69(nums):
+    result = 0
+    add = True
+    for num in nums:
+        while add:
+            if num != 6:
+                result += num
+                break
+            else:
+                add = False
+        while not add:
+            if num != 9:
+                break
+            else:
+                add = True
+                break
+    return result
