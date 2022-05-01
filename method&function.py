0669f53f4d6dc1ba0d5a6ebf2ad86731bac648a7
Atsuki Uchida <csatsfapsfu2930@gmail.com>
2022-05-01 15:59:20 -0700

    solved spy problem

--- method&function.py
+++ method&function.py
@@ -132,3 +132,17 @@ def summer_69(nums):
                 add = True
                 break
     return result
+
+
+"""SPY GAME: Write a function that takes in a list of integers and returns True if it contains 007 in order
+ spy_game([1,2,4,0,0,7,5]) --> True
+ spy_game([1,0,2,4,0,5,7]) --> True
+ spy_game([1,7,2,0,4,5,0]) --> False"""
+
+
+def spy_game(nums):
+    code = [0, 0, 7, 'x']
+    for num in nums:
+        if num == code[0]:
+            code.pop(0)
+    return len(code) == 1
