6b7317cbedd746f32dc8127158248969307bc2b9
Atsuki Uchida <csatsfapsfu2930@gmail.com>
2022-06-02 12:19:09 +0900

    added some oop problem and solved them in python

---  Function/OOP.py
+++  Function/OOP.py
@@ -3,20 +3,18 @@ he Line class methods to accept coordinates as a pair of tuples
 and return the slope and distance of the line.
 """
 
-class Line:
 
+class Line:
 
     def __init__(self, coor1, coor2):
         self.coor1 = coor1
         self.coor2 = coor2
-
 
     def distance(self):
         x1, y1 = self.coor1
         x2, y2 = self.coor2
 
         return ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5
-
 
     def slope(self):
         x1, y1 = self.coor1
@@ -25,6 +23,7 @@ class Line:
         return ((y2 - y1) / (x2 - x1))
 
     # EXAMPLE OUTPUT
+
 
 coordinate1 = (3, 2)
 coordinate2 = (8, 10)
@@ -48,6 +47,62 @@ class Cylinder:
         return (2 * top) + (2 * 3.14 * self.radius * self.height)
 
 
-c = Cylinder(2,3)
+c = Cylinder(2, 3)
 c.volume()
 c.surface_area()
+
+"""
+create a bank account class that has two attributes:
+
+owner
+balance
+and two methods:
+
+deposit
+withdraw
+As an added requirement, withdrawals may not exceed the available balance.
+
+Instantiate your class, make several deposits and withdrawals, and test to make sure the account can't be overdrawn."""
+
+
+class Account:
+
+    def __init__(self, owner, balance=0):
+        self.owner = owner
+        self.balance = balance
+
+    def __str__(self):
+        return f'Account owner:   {self.owner}\nAccount balance: ${self.balance}'
+
+    def deposit(self, dep_amt):
+        self.balance += dep_amt
+        print('Deposit Accepted')
+
+    def withdraw(self, wd_amt):
+        if self.balance >= wd_amt:
+            self.balance -= wd_amt
+            print('Withdrawal Accepted')
+        else:
+            print('Funds Unavailable!')
+
+    # 1. Instantiate the class
+
+
+acct1 = Account('Atsuki', 100)
+
+# 2. Print the object
+print(acct1)
+
+# 3. Show the account owner attribute
+acct1.owner
+
+# 4. Show the account balance attribute
+acct1.balance
+
+# 5. Make a series of deposits and withdrawals
+acct1.deposit(50)
+
+acct1.withdraw(75)
+
+# 6. Make a withdrawal that exceeds the available balance
+acct1.withdraw(500)
