9c2cef01e943554f7acd6f51c3ad04ad43dae8ad
Atsuki Uchida <csatsfapsfu2930@gmail.com>
2022-06-09 11:02:42 +0900

    Added more class and OOP in python

---  Function/OOP.py
+++  Function/OOP.py
@@ -131,3 +131,14 @@ class Dog(Animal):
 
     def sound(self):
         print("Wuff")
+
+
+class Student:
+    def __init__(self, names):
+        self.names = names
+
+    def __len__(self):
+        return len(self.names)
+
+    def __str__(self):
+        return f"{self.names}
