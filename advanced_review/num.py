fd1d9e18a3aa806d7527f02a2f086347c9bbffb6
Atsuki Uchida <csatsfapsfu2930@gmail.com>
2022-07-17 14:12:52 +0900

    reviwed builtin num module

---
+++ advanced_review/num.py
@@ -0,0 +1,31 @@
+# check hex values
+
+print(hex(246))
+
+print(hex(512))
+
+# binary check
+
+print(bin(1234))
+
+print(bin(128))
+
+print(bin(512))
+
+# exp
+
+print(pow(3,4))
+
+print(pow(3,4,5)) #return 1 because? (x^y)%z
+
+#abs 
+
+print(abs(-3.14))
+
+print(abs(3))
+
+# rounds
+
+print(round(3,2))
+
+print(round(395,-2))
