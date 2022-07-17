77ad638c7bc79a1b21cf2646b87052de8ed3cf2c
Atsuki Uchida <csatsfapsfu2930@gmail.com>
2022-07-17 14:19:46 +0900

    played with strings method

---
+++ advanced_review/strings.py
@@ -0,0 +1,12 @@
+strings = "hello world"
+
+print(strings.capitalize())
+print(strings.upper())
+print(strings.lower())
+
+print(strings.count('o'))
+print(strings.find('o')) # good to way to find the first index of something
+
+print(strings.center(20,'a')) # length 20 with aaa around
+strings.split()
+print(strings.partition('l'))
